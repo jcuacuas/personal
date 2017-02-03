f254fb341bd164fc5b9bbd23f8f26f97b88d2b41
Jose Cuacuas <jcuacuas.ptech@gmail.com>
2017-02-02 21:28:37 -0500

    added more animations

--- js/app.js
+++ js/app.js
@@ -1,1 +1,16 @@
-$(document).foundation()
+$(document).foundation(){
+  document.addEventListener('DOMContentLoaded', function(){
+      var trigger = new ScrollTrigger({
+        toggle: {
+          visible: 'visibleClass',
+          hidden: 'hiddenClass'
+        },
+        offset: {
+          x: 0,
+          y: 20
+        },
+        addHeight: true,
+        once: true
+      }, document.body, window);
+  });
+}
